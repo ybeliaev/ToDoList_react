90fbb0c046926f283da09552d8bb627fdca64868
Yura <ybeliaev@gmail.com>
2019-01-26 12:31:04 +0200

    props.label

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,11 +1,7 @@
 import React from "react";
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? "tomato" : "black"
-  };
-
-  return <span style={style}>{label}</span>;
+const TodoListItem = ({ label, important }) => {
+  return <span>{label}</span>;
 };
 
 export default TodoListItem;
