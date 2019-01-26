6a937376106e45944dd02024c681db3e1c1f9756
Yura <ybeliaev@gmail.com>
2019-01-26 12:15:53 +0200

    TodoListItem to do

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+import TodoListItem from "../todo-list-item";
+
 const TodoList = () => {
   return (
     <ul>
-      <li>some text</li>
+      <li>
+        <TodoListItem label="Drink Coffee" />
+      </li>
+      <li>
+        <TodoListItem label="Build React App" important />
+      </li>
     </ul>
   );
 };
