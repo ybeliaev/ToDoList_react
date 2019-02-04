97b81135b791f46b8aeef95d3f5de13014ad5d6a
Yura <ybeliaev@gmail.com>
2019-02-04 15:26:07 +0200

    add important class to state

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -11,19 +11,27 @@ export default class TodoListItem extends Component {
       done: true
     });
   };
+  onMarkImportant = () => {
+    this.setState({
+      important: true
+    });
+  };
   render() {
-    const { label, important } = this.props;
-    const { done } = this.state;
+    const { label } = this.props;
+    const { done, important } = this.state;
     let classNames = "todo-list-item";
     if (done) {
       classNames += " done";
+    }
+    if (important) {
+      classNames += " important";
     }
     return (
       <span className={classNames}>
         <span className="todo-list-item-label" onClick={this.onLabelClick}>
           {label}
         </span>
-        <button className="btn">
+        <button className="btn" onClick={this.onMarkImportant}>
           <i className="fa fa-exclamation" />
         </button>
         <button className="btn">
