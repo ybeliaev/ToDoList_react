f354db694f98576836868eb4e36bc5dbed94e946
Yura <ybeliaev@gmail.com>
2019-02-05 22:54:13 +0200

    full event delete

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,12 +9,23 @@ import "./app.css";
 
 export default class App extends Component {
   state = {
-    tododata: [
+    todoData: [
       { label: "Drink Coffee", important: false, id: 1 },
       { label: "Go in for sports", important: false, id: 2 },
       { label: "Study Bible", important: true, id: 3 },
       { label: "Feed the cat", important: false, id: 4 }
     ]
+  };
+  makeDelete = id => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex(el => el.id === id);
+      const before = todoData.slice(0, idx);
+      const after = todoData.slice(idx + 1);
+      const newArr = [...before, ...after];
+      return {
+        tododata: newArr
+      };
+    });
   };
 
   render() {
@@ -25,10 +36,8 @@ export default class App extends Component {
           <SeachPanel />
           <ItemStatusFilter />
         </div>
-        <TodoList todos={this.state.tododata} onDeletedItem={this.makeDelete} />
+        <TodoList todos={this.state.todoData} onDeleted={this.makeDelete} />
       </div>
     );
   }
 }
-
-App;
